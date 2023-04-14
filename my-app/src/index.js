623cb68372561081917af98732c536cb8c64320a
Samira77-ss <samiraseyfii0709@gmail.com>
2023-04-14 11:26:39 +0330

    Updated some new changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Weather />
+    <div className="App">
+      <Weather />
+    </div>
   </React.StrictMode>
 );
 
